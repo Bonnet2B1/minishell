a86dd05670cd68593d60e979f791e32075f8cccb
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-09-18 15:15:30 +0200

    env_var works with db quotes

--- srcs/parsing/env_variable.c
+++ srcs/parsing/env_variable.c
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 21:26:48 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/09/17 22:18:35 by edelarbr         ###   ########.fr       */
+/*   Updated: 2023/09/18 15:10:35 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	there_is_a_env_var(char **line)
 	while (line[0][++i])
 	{
 		if (line[0][i] == '$' && ft_isenvchar(line[0][i + 1])
-			&& line[1][i] == '0' && line[1][i + 1] == '0')
+			&& ((line[1][i] == '0' && line[1][i + 1] == '0') || (line[1][i] == '\"' && line[1][i + 1] == '\"')))
 			return (1);
 	}
 	return (0);
@@ -36,7 +36,7 @@ char	**take_before(char **line)
 	while (line[0][++len] && line[1][len] != '\0')
 	{
 		if (line[0][len] == '$' && ft_isenvchar(line[0][len + 1])
-			&& line[1][len] == '0' && line[1][len + 1] == '0')
+			&& ((line[1][len] == '0' && line[1][len + 1] == '0') || (line[1][len] == '\"' && line[1][len + 1] == '\"')))
 			break ;
 	}
 	before[0] = ft_substr(line[0], 0, len);
@@ -44,28 +44,32 @@ char	**take_before(char **line)
 	return (before);
 }
 
-char	*take_var(char **line)
+char	**take_var(char **line)
 {
 	int		start;
 	int		len;
-	char	*var;
+	char	**var;
 
+	var = malloc(sizeof(char *) * 3);
+	var[2] = NULL;
 	start = -1;
 	while (line[0][++start])
 		if (line[0][start] == '$' && ft_isenvchar(line[0][start + 1])
-			&& line[1][start] == '0' && line[1][start + 1] == '0')
+			&& ((line[1][start] == '0' && line[1][start + 1] == '0') || (line[1][start] == '\"' && line[1][start + 1] == '\"')))
 			break ;
 	if (line[0][start] == '$')
 		start++;
 	len = 0;
-	while (ft_isenvchar(line[0][start + len]) && line[1][start + len] == '0')
+	while (ft_isenvchar(line[0][start + len]) && (line[1][start + len] == '0' || line[1][start + len] == '\"'))
 		len++;
-	var = ft_substr(line[0], start, len);
-	printf("var: \"%s\"\n", var);
+	var[0] = ft_substr(line[0], start, len);
+	var[1] = ft_substr(line[1], start, len);
+	printf("var[0]: \"%s\"\n", var[0]);
+	printf("var[1]: \"%s\"\n", var[1]);
 	return (var);
 }
 
-char	**take_replacement(char *var, char **env)
+char	**take_replacement(char **var, char **env)
 {
 	int		i;
 	char	*joined;
@@ -73,7 +77,7 @@ char	**take_replacement(char *var, char **env)
 
 	replacement = malloc(sizeof(char *) * 2);
 	replacement[0] = NULL;
-	joined = ft_strjoin(var, "=");
+	joined = ft_strjoin(var[0], "=");
 	i = -1;
 	while (env[++i])
 	{
@@ -81,13 +85,14 @@ char	**take_replacement(char *var, char **env)
 		{
 			replacement[0] = ft_substr(env[i], ft_strlen(joined),
 					ft_strlen(env[i]) - ft_strlen(joined));
-			replacement[1] = ft_memset(malloc(sizeof(char) * (ft_strlen(replacement[0]) + 1)), '0', ft_strlen(replacement[0]));
+			replacement[1] = ft_memset(malloc(sizeof(char) * (ft_strlen(replacement[0]) + 1)), var[1][0], ft_strlen(replacement[0]));
 			replacement[1][ft_strlen(replacement[0])] = '\0';
 			return (free(joined), replacement);
 		}
 	}
 	replacement[0] = ft_strdup("");
 	replacement[1] = ft_strdup("");
+	freetab(var);
 	return (free(joined), replacement);
 }
 
@@ -95,17 +100,22 @@ char	**take_after(char **line)
 {
 	int		start;
 	char	**after;
+	char	**var;
 
 	after = malloc(sizeof(char *) * 2);
 	start = -1;
 	while (line[0][++start])
 	{
 		if (line[0][start] == '$' && ft_isenvchar(line[0][start + 1])
-			&& line[1][start] == '0' && line[1][start + 1] == '0')
+			&& ((line[1][start] == '0' && line[1][start + 1] == '0') || (line[1][start] == '\"' && line[1][start + 1] == '\"')))
 			break ;
 	}
 	if (line[0][start] == '$')
-		start += ft_strlen(take_var(line)) + 1;
+	{
+		var = take_var(line);
+		start += ft_strlen(var[0]) + 1;
+		freetab(var);
+	}
 	after[0] = ft_substr(line[0], start, 2147483647);
 	after[1] = ft_substr(line[1], start, 2147483647);
 	return (after);

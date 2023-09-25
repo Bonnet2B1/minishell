50f22068a37107dd631cc459468b814341a15633
rbrendle <109989668+nevaspid@users.noreply.github.com>
2023-09-25 14:12:55 +0200

    ft_cd : ok

---
+++ srcs/builtins/ft_cd.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 17:54:21 by gloms             #+#    #+#             */
+/*   Updated: 2023/09/25 14:10:47 by gloms            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	find(t_shell_memory *env, char *find, int len)
+{
+	int	i;
+	int	len_env;
+
+	i = -1;
+	len_env = -1;
+	while (env->env[++len_env])
+		;
+	while (ft_strncmp(env->env[++i], find, len))
+	{
+		if (i == len_env)
+			return (-1);
+	}
+	return (i);
+}
+
+int	cd_root(t_shell_memory *env)
+{
+	int	index_pwd;
+
+	index_pwd = find(env, "PWD=", 4);
+	if (chdir("/") == -1)
+	{
+		perror("cd");
+		return (-1);
+	}
+	free(env->env[index_pwd]);
+	env->env[index_pwd] = ft_strjoin("PWD=", getcwd(NULL, 0));
+	return (0);
+}
+
+int	change_path(t_shell_memory *env, char *args)
+{
+	char	*pat;
+	int		index_pwd;
+
+	index_pwd = find(env, "PWD=", 4);
+	pat = ft_strjoin(getcwd(NULL, 0), "/");
+	pat = ft_strjoin_free_s1(pat, ft_substr(args, 0, ft_strlen(args)));
+	if (chdir(pat) == -1)
+	{
+		perror("cd");
+		return (-1);
+	}
+	free(env->env[index_pwd]);
+	env->env[index_pwd] = ft_strjoin("PWD=", getcwd(NULL, 0));
+	free(pat);
+	return (0);
+}
+
+int	cd_home(t_shell_memory *env)
+{
+	char	*home;
+	int		len;
+	int		index_home;
+
+	index_home = find(env, "HOME=", 5);
+	len = ft_strlen(env->env[index_home]);
+	home = ft_substr(env->env[index_home], 5, len - 5);
+	if (!home)
+	{
+		printf("minishell: cd: HOME not set\n");
+		return (-1);
+	}
+	if (chdir(home) == -1)
+	{
+		perror("cd");
+		return (-1);
+	}
+	free(env->env[index_home]);
+	env->env[index_home] = ft_strjoin("PWD=", getcwd(NULL, 0));
+	free(home);
+	return (0);
+}
+
+int	ft_cd(t_shell_memory *env, char **args)
+{
+	int		ind;
+	char	*tmp;
+
+	((void)0, tmp = getcwd(NULL, 0), ind = find(env, "OLDPWD=", 7));
+	if (args[1])
+	{
+		if (!ft_strncmp(args[1], "~", 1))
+		{
+			args[1] = ft_substr(args[1], 1, ft_strlen(args[1]) - 1);
+			if (cd_home(env) < 0)
+				ft_exit(1);
+		}
+		else if (!ft_strncmp(args[1], "/", 1))
+		{
+			args[1] = ft_substr(args[1], 1, ft_strlen(args[1]) - 1);
+			if (cd_root(env) < 0)
+				ft_exit(1);
+		}
+		change_path(env, args[1]);
+	}
+	else
+		cd_home(env);
+	env->env[ind] = ft_strjoin(ft_substr_free(env->env[ind], 0, 7), tmp);
+	free(tmp);
+	ft_exit(0);
+}

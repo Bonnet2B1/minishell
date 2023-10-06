de2591a425e7fece7678b6e9cc8a152ce98a8830
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-10-06 17:02:33 +0200

    new echo, work with "echo -nnnn -n -nnn -nm fdsa"

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -3,71 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
+/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:05:06 by gloms             #+#    #+#             */
-/*   Updated: 2023/10/05 23:52:54 by gloms            ###   ########.fr       */
+/*   Updated: 2023/10/06 17:01:20 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	check_args(char *str)
+int	is_flag(char *str)
 {
 	int	i;
 
 	i = 1;
+	if (str[0] != '-')
+		return (0);
 	while (str[i])
 	{
-		if (str[0] != '-')
-			return (0);
 		if (str[i] != 'n')
 			return (0);
-		i++
+		i++;
 	}
 	return (i);
 }
 
-void	print_no_nl(char **args, int ac)
-{
-	int	i;
-
-	i = 2;
-	while (args[i])
-	{
-		if (i == ac - 1)
-		{
-			printf("%s", args[i]);
-			break ;
-		}
-		printf("%s ", args[i]);
-		i++;
-	}
-	return ;
-}
-
 void	ft_echo(t_shell_memory *data, char **args)
 {
-	int	ac;
 	int	i;
 
-	i = 0;
-	ac = ft_tablen(args);
-	if (ac == 1)
-		return (write (1, "\n", 1), free_n_exit(data, 0));
-	else if (ac == 2 && !ft_strcmp(args[1], "-n"))
-		return (free_n_exit(data, 0));
-	else if (ac > 2 && !ft_strcmp(args[1], "-n"))
+	i = 1;
+	while (is_flag(args[i]))
+		i++;
+	if (i > 1)
 	{
-		print_no_nl(args, ac);
-		free_n_exit(data, 0);
+		while (args[i])
+		{
+			printf("%s", args[i++]);
+			if (args[i])
+				printf(" ");
+		}
 	}
-	while (args[++i] && i < ac - 1)
+	else
 	{
-		printf("%s", args[i]);
-		if (args[i + 1])
-			printf(" ");
+		while (args[i])
+		{
+			printf("%s", args[i++]);
+			if (args[i])
+				printf(" ");
+		}
+		printf("\n");
 	}
-	printf("%s\n", args[i]);
 	free_n_exit(data, 0);
 }
